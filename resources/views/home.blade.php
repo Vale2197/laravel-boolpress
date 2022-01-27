4547c3388edc596f0c30311a8f767a602ae2dadf
Vale2197 <valentinatrebbi97@gmail.com>
2022-01-27 17:44:52 +0100

    aggiustata view 'welcome'

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,8 +2,13 @@
 
 @section('content')
 
-    <h1>
-            WELCOME
-    </h1>
-    
+<div class="px-4 py-5 my-5 text-center">
+    <i class="fas fa-smile d-block mx-auto mb-4"></i>
+    <h1 class="display-5 fw-bold"> WELCOME! </h1>
+    <div class="col-lg-6 mx-auto">
+        <p class="lead mb-4">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae magnam numquam facilis in dolore libero, voluptates minima, dolorem dolorum eveniet nisi quam illo? Dicta sequi cupiditate laborum quae sint placeat!
+        </p>
+    </div>
+  </div>
 @endsection
